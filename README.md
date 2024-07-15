cfd22dde1bc9532bda1e3588db62d39f3efbad12
MOC <moc4418@naver.com>
2024-07-15 15:03:35 +0900

    0715

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # TIL
 
-* Git 카테고리
-    * [Git 기본 내용](./git.md)
+- Python
+    * [0715](Python/0715.md)
---
+++ asdf.py
@@ -0,0 +1,10 @@
+korean='한글'
+english='english'
+number=3
+
+print(korean)
+print(english)
+print(number)
+
+print(f'{korean}과 {english}')
+print(3*korean)
